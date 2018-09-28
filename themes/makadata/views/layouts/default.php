06566ab9e048f99b016ed2852b26f32cec4e33e9
icalramdhan <faisal.ramadhan666@gmail.com>
2018-09-28 17:43:02 +0700

    add url sign up

--- themes/makadata/views/layouts/default.php
+++ themes/makadata/views/layouts/default.php
@@ -56,7 +56,7 @@
                     <a href="#" data-toggle="modal" data-target="#login-modal" class="login-btn">
                         <i class="fa fa-sign-in"></i><span class="d-none d-md-inline-block">Sign In</span>
                     </a>
-                    <a href="customer-register.html" class="signup-btn">
+                    <a href="<?php echo base_url('auth/register'); ?>" class="signup-btn">
                       <i class="fa fa-user"></i><span class="d-none d-md-inline-block">Sign Up</span>
                     </a>
                   <?php } else { ?>
